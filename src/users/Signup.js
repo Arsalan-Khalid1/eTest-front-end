6299f24c108d581f923017053505a79faaa016b4
DareDefyMe <70864245+DareDefyMe@users.noreply.github.com>
2020-11-06 18:50:45 +0500

    grabing data from fields

--- src/users/Signup.js
+++ src/users/Signup.js
@@ -1,26 +1,62 @@
-import react from 'react';
+import react, {useState} from 'react';
 import {API} from '../config';
 const Signup = () => {
+  const [values, setValues] = useState ({
+    name: '',
+    email: '',
+    password: '',
+    error: '',
+    success: false,
+  });
+
+  const {name, email, password} = values;
+
+  const handleChange = name => event => {
+    setValues ({...values, error: false, [name]: event.target.value});
+  };
+
+  const signup = (name, email, password) => {
+    console.log (name, email, password);
+  };
+  const clickSubmit = event => {
+    event.preventDefault ();
+    signup (name, email, password);
+  };
+
   const signUpForm = () => {
     return (
       <div className="container col-md-8 offset-md-2">
         <form>
           <div className="form-group">
             <label className="text-muted">Name</label>
-            <input type="text" className="form-control" />
+            <input
+              onChange={handleChange ('name')}
+              type="text"
+              className="form-control"
+            />
           </div>
 
           <div className="form-group">
             <label className="text-muted">Email</label>
-            <input type="email" className="form-control" />
+            <input
+              onChange={handleChange ('email')}
+              type="email"
+              className="form-control"
+            />
           </div>
 
           <div className="form-group">
             <label className="text-muted">Password</label>
-            <input type="password" className="form-control" />
+            <input
+              onChange={handleChange ('paswword')}
+              type="password"
+              className="form-control"
+            />
           </div>
 
-          <button className="btn btn-primary">Submit</button>
+          <button onClick={clickSubmit} className="btn btn-primary">
+            Submit
+          </button>
 
         </form>
 
