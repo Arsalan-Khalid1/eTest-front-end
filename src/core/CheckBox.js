3c80a2d24f3854d6f2b1b508cc87a314082c5b08
DareDefyMe <70864245+DareDefyMe@users.noreply.github.com>
2020-11-26 18:29:04 +0500

    filter checkbox

--- src/core/CheckBox.js
+++ src/core/CheckBox.js
@@ -1,9 +1,30 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 
 const CheckBox = ({categories}) => {
+  const [checked, setChecked] = useState ([]);
+
+  const handleToggle = category => () => {
+    const currCategoryId = checked.indexOf (category);
+    const newCheckedCategoryId = [...checked];
+
+    if (currCategoryId === -1) {
+      newCheckedCategoryId.push (category);
+    } else {
+      newCheckedCategoryId.splice (currCategoryId, 1);
+    }
+
+    console.log (newCheckedCategoryId);
+    setChecked (newCheckedCategoryId);
+  };
+
   return categories.map ((category, index) => (
     <li key={index} className="list-unstyled">
-      <input type="checkbox" className="form-check-input" />
+      <input
+        className="form-check-input"
+        onChange={handleToggle (category._id)}
+        value={checked.indexOf (category._id === -1)}
+        type="checkbox"
+      />
       <label className="form-check-label">{category.name}</label>
     </li>
   ));
